4f28d2deef6dbc4d3c25a4044bf09ab82c0ac46a
Tentoze <kuba.rosinski0@o2.pl>
2023-10-14 11:12:37 +0200

    Added Spring Project

---
+++ backend/AccessGoods/src/main/java/accessgoods/AccessGoodsApplication.java
@@ -0,0 +1,13 @@
+package accessgoods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessGoodsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccessGoodsApplication.class, args);
+    }
+
+}
